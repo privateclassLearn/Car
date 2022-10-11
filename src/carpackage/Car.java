e7e923784930d97bf4ec344ba764749133a886d2
Ulrich Rosenbaum <urosenbaum@gmail.com>
2022-10-11 15:43:47 +0200

    Changed comments

--- src/carpackage/Car.java
+++ src/carpackage/Car.java
@@ -10,7 +10,6 @@ import static carpackage.CarGame.game;
 
 public class Car {
     public static void main(String[] args) {
-// Calling the class CarGame.java
         playGame();
         CarFactory myCar = new CarFactory();
         myCar.producer = "Dacia";
@@ -79,7 +78,7 @@ public class Car {
         return power0 + power1 + power2;
     }
 
-    // Below ist the car game method
+    // Below ist the car game playing question method
     static void playGame() {
         System.out.println("Do you want o play the game? y/n: ");
         Scanner playGame = new Scanner(System.in);
@@ -87,50 +86,48 @@ public class Car {
         System.out.println("Your answer has been: " + playing);
         if (Objects.equals(playing, "y")) {
             game();
+        } else {
+            System.out.println("Thank you!");
         }
-                    else {
-                System.out.println("Thank you!");
-            }
     }
+
+    // Below ist the car game method
     static void game() {
 
-
-            System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-            System.out.println("+                   The Car Power Guessing Game                                 +");
-            System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-            System.out.println("Your neighbor's car has 50kw to 150kw.");
-            Scanner scanner = new Scanner(System.in);
-            Random rand = new Random();
-            int randomPower = rand.nextInt((150 - 50)) + 50;
-            boolean playerGuessedCorrectly = false;
-            do {
-                System.out.print("Please guess the power of your neighbor's car: ");
-                int playerguess = scanner.nextInt();
-                if (playerguess == randomPower) {
-                    System.out.println("Correct! You guessed the right power and win!");
-                    playerGuessedCorrectly = true;
-                } else if (randomPower > playerguess) {
-                    System.out.println("Not yet! The power is higher!");
-                } else {
-                    System.out.println("Not yet! The power ist lower!");
-                }
-            }
-            while (!playerGuessedCorrectly);
-            System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-            System.out.println("+                   Game over                                                   +");
-            System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-
-            // Scanner playGame = new Scanner(System.in);
-            System.out.println("Do you want o play the game? y/n: ");
-            Scanner playGame = new Scanner(System.in);
-            String playingEnd = playGame.nextLine();
-            System.out.println("Your answer has been: " + playingEnd);
-            if (Objects.equals(playingEnd, "y")) {
-                game();
-            }
-            else {
-                System.out.println("Thank you!");
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("+                   The Car Power Guessing Game                                 +");
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("Your neighbor's car has 50kw to 150kw.");
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        int randomPower = rand.nextInt((150 - 50)) + 50;
+        boolean playerGuessedCorrectly = false;
+        do {
+            System.out.print("Please guess the power of your neighbor's car: ");
+            int playerguess = scanner.nextInt();
+            if (playerguess == randomPower) {
+                System.out.println("Correct! You guessed the right power and win!");
+                playerGuessedCorrectly = true;
+            } else if (randomPower > playerguess) {
+                System.out.println("Not yet! The power is higher!");
+            } else {
+                System.out.println("Not yet! The power ist lower!");
             }
         }
+        while (!playerGuessedCorrectly);
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("+                   Game over                                                   +");
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 
+        // Scanner playGame = new Scanner(System.in);
+        System.out.println("Do you want o play the game? y/n: ");
+        Scanner playGame = new Scanner(System.in);
+        String playingEnd = playGame.nextLine();
+        System.out.println("Your answer has been: " + playingEnd);
+        if (Objects.equals(playingEnd, "y")) {
+            game();
+        } else {
+            System.out.println("Thank you!");
+        }
     }
+}
