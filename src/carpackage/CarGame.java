852dc6e12605ef20f87dceb6415cfcc54d0ff7c8
Ulrich Rosenbaum <urosenbaum@gmail.com>
2022-10-12 09:25:22 +0200

    Typo in for loop corrected

--- src/carpackage/CarGame.java
+++ src/carpackage/CarGame.java
@@ -48,7 +48,7 @@ public class CarGame {
     static void forLoop() {
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
-                System.out.println(i + " * " + j + " = " + (1 * j));
+                System.out.println(i + " * " + j + " = " + (i * j));
             }
         }
     }
